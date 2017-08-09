b8363537416dadd3ba61415ef73c36d66a93e1a3
RyoNkmr <ryonakamuraryo@gmail.com>
2017-08-09 16:48:43 +0900

    subjects

--- plugins/responses.py
+++ plugins/responses.py
@@ -4,3 +4,16 @@ from slackbot.bot import respond_to
 def what(message):
     message.reply('Typescript')
 
+subjects = []
+
+@respond_to('お題:(.*)')
+def add_subject(message, subs):
+    global subjects
+    subjects += subs.split()
+
+@respond_to('お題候補')
+def add_subject(message):
+    global subjects
+    message.react('ok_woman')
+    reply = 'お題は' + subjects.count() + '個あります。\n```' + ', '.join(subjects) + '```'
+    message.reply(reply)
