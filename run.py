e1e376ac4ce65c3cd07ab82d87ef44db7daab959
RyoNkmr <ryonakamuraryo@gmail.com>
2017-08-09 14:19:20 +0900

    add some reaction

--- run.py
+++ run.py
@@ -6,3 +6,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+@respond_to('好きな言語は？')
+def what(message):
+    message.reply('Typescript')
+
+@listen_to('言語')
+def iine(message):
+    message.react('Typescriptが一番いいに決まってるだろ')
+
+@listen_to('型')
+def iine(message):
+    message.react('型安全')
